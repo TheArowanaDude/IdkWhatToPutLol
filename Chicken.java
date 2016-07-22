0f2ced4f27f7b510b00cf1c09939759d047aa9f3
TheArowanaDude <micropachycephalosaurs@gmail.com>
2016-07-22 00:08:08 -0500

    Add files via upload
    I am currently learning the concept of polymorphism and inheritance. 
    I am hoping to use these skills to develop a simple graphics user interface program.

---
+++ Chicken.java
@@ -0,0 +1,29 @@
+	class Animals{ 
+		String latarea; 
+		int lol; 
+		
+		
+		
+		} 
+		
+		public class Chicken{ 
+			public static void main(String[] args){ 
+			Animals idklol = new Animals(); 
+			
+			idklol.lol = 75; 
+			
+			System.out.println(idklol.lol);
+			
+			
+			
+			}
+		
+		
+		
+		
+		}
+	
+	
+	
+	
+	
