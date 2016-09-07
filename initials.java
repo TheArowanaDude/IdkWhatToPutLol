bb6956326e61f6390775c164aec9ae0d62e3705d
Rayyan Memon <micropachycephalosaurs@gmail.com>
2016-09-06 19:23:03 -0500

    Add files via upload
    Inspired by Harvard's CS50 "Initials" homework assignment, I wrote this program in java which takes the user's input and prints out the initials of the name inputted. 
    This helped me strengthen my concepts on if statements and while loops(which feel more like if loops).

---
+++ initials.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class initials{
+  public static void main(String args[]){
+  Scanner input = new Scanner(System.in);
+  System.out.println("Please input your name");
+  String name = input.nextLine();
+
+      int i = 0;
+
+      while( name.charAt(i) == ' ' ){
+            i++;
+          }
+          System.out.print(name.charAt(i));
+
+          for(int x = i; x < name.length() - 1; x++){
+              while( name.charAt(x) == ' ' ){
+                x++;
+
+                if(name.charAt(x) != ' ' ){
+                  System.out.print(name.charAt(x));
+
+                }
+
+              }
+
+
+          }
+
+
+
+            }
+
+
+            }
