308c6883bdb1fe87d7b1cf6809402e77dd3b41de
Rayyan Memon <micropachycephalosaurs@gmail.com>
2020-08-12 11:24:35 -0500

    Add files via upload
    weather station with sensehat hardware

---
+++ weatherStation.py
@@ -0,0 +1,22 @@
+from sense_hat import SenseHat
+import time
+import sys 
+
+sense = SenseHat()
+sense.clear()
+
+try:
+	while True:
+		temp = sense.get_temperature()
+		temp = 1.8 * round(temp, 1) + 32
+		print("Temperature F: ", temp)
+		humidity = sense.get_humidity()
+		humidity = round(humidity, 1)
+		print("Humidity: ", humidity)
+		pressure = sense.get_pressure()
+		pressure = round(pressure, 1)
+		print("Pressure: ", pressure)
+		time.sleep(1)
+except KeyboardInterrupt: 
+	pass
+
